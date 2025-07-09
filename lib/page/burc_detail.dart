36b6f631c5c3f1b5e6d3502b429d3ac787a19154
Ali Kutlu <kutluali@outlook.com.tr>
2025-07-09 09:33:24 +0300

    commit

--- lib/page/burc_detail.dart
+++ lib/page/burc_detail.dart
@@ -1,12 +1,50 @@
 import 'package:burc_app/Model/burc.dart';
 import 'package:flutter/material.dart';
+import 'package:palette_generator/palette_generator.dart';
 
-class BurcDetail extends StatelessWidget {
+class BurcDetail extends StatefulWidget {
   final Burc secilenBurc;
   const BurcDetail({super.key, required this.secilenBurc});
 
   @override
+  State<BurcDetail> createState() => _BurcDetailState();
+}
+
+class _BurcDetailState extends State<BurcDetail> {
+  Color appBarRengi = Colors.pink;
+  late PaletteGenerator _generator;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 250,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text("${widget.secilenBurc.burcAdi} Burcu ve Özellikleri"),
+              background: Image.asset(
+                "images/${widget.secilenBurc.burcBuyukResim}",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              margin: EdgeInsets.all(16),
+              padding: EdgeInsets.all(8),
+              child: SingleChildScrollView(
+                child: Text(
+                  widget.secilenBurc.burcDetay,
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/widgets/burc_item.dart
+++ lib/widgets/burc_item.dart
@@ -1,4 +1,5 @@
 import 'package:burc_app/Model/burc.dart';
+import 'package:burc_app/page/burc_detail.dart';
 import 'package:flutter/material.dart';
 
 class BurcItem extends StatelessWidget {
@@ -14,6 +15,13 @@ class BurcItem extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.symmetric(vertical: 6),
           child: ListTile(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => BurcDetail(secilenBurc: gelenBurc),
+                ),
+              );
+            },
             leading: Image.asset("images/${gelenBurc.burcKucukResim}"),
             title: Text(gelenBurc.burcAdi),
             subtitle: Text(gelenBurc.burcTarihleri),
